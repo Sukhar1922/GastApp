e56b71c44161ca5ca05f76ad1208d79422903b77
Sukhar <s.a.v.04@ya.ru>
2024-12-04 14:39:09 +0300

    test branch init

--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.db import connection
+
 # Create your views here.
 
 def index(req):
     return HttpResponse('<h1>Пиривет! У нас Периф</h1>')
 
 def testPage(req):
-    return HttpResponse('<h1>Пиривет! У нас Периф И теста</h1>')
+    return render(req, 'main/test.html')
